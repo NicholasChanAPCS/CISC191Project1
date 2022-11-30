5e4547195154e54b998d42c9eda4110bf58cd757
myname <nicho@LAPTOP-QKDGM9OH>
2022-11-30 12:34:48 -0800

    UML and purchase class

---
+++ src/Purchase.java
@@ -0,0 +1,13 @@
+
+public class Purchase extends Billing 
+{
+	int advil = 10;
+	int medicalTape = 5; 
+	int acetaminophen = 7;
+	int asprin = 11;
+	int rubbingAlcohol = 8;
+	int inhaler = 150;
+	int epipen = 650;
+	int bandAids = 5;
+
+}
